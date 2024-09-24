3d6cedcc21ec3b4f5c0975557fb784e2dbd9ed83
Krishnendu <krishnendudalui@gmail.com>
2024-09-25 00:05:44 +0530

    c onfigure your CodeIgniter project for both localhost and the live website

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/1_CI/hotel/';
+// $config['base_url'] = 'http://localhost/1_CI/hotel/';
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $config['base_url'] = 'http://localhost/1_CI/hotel/';
+} else {
+    $config['base_url'] = 'https://krishnendudalui.in.net/hotel_ci/';
+}
+
 
 /*
 |--------------------------------------------------------------------------
